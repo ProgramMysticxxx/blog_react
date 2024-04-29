21396ebd93b6ae95e87eeb22be397128c6a7fa1a
p-ukd <118949473+P-UKD@users.noreply.github.com>
2024-04-29 23:49:33 +0300

    + editor

--- src/components/bloging/Bloging.js
+++ src/components/bloging/Bloging.js
@@ -1,5 +1,6 @@
+import JoditEditor from 'jodit-react';
 import './bloging.scss';
-import RichTextEditor from '../editor/RichTextEditor';
+// import RichTextEditor from '../editor/RichTextEditor';
 
 
 function Bloging() {
@@ -9,10 +10,11 @@ function Bloging() {
                 <h1 className="title title_blocks title_mb-30">Creating your own article is easy</h1>
                 <p className="bloging__descr">Here, you can create your own articles, share your thoughts and ideas, and inform and educate other users. Give free rein to your creativity and experience to make this space vibrant and engaging!</p>
                 <h2 className="title title_mb-30">Create article</h2>
-                <form className="form editor__form">
+                <div className="form editor__form" style={{flexDirection: 'column', width: '100%', height: '500px'}}>
                     <input type="text" name="title" placeholder="Title your article" className="editor__title"/>
-                    <RichTextEditor/>
-                </form>
+                    {/* <RichTextEditor/> */}
+                    <JoditEditor />
+                </div>
             </div>
         </section>
     )
