cb20bc65893536cb44c124c2d60a18e063771b95
Nazar Talaievych <losiara345@gmail.com>
2024-04-30 06:59:16 +0300

    create-article

--- src/components/bloging/Bloging.js
+++ src/components/bloging/Bloging.js
@@ -1,6 +1,5 @@
 import JoditEditor from 'jodit-react';
 import './bloging.scss';
-// import RichTextEditor from '../editor/RichTextEditor';
 
 
 function Bloging() {
@@ -10,10 +9,18 @@ function Bloging() {
                 <h1 className="title title_blocks title_mb-30">Creating your own article is easy</h1>
                 <p className="bloging__descr">Here, you can create your own articles, share your thoughts and ideas, and inform and educate other users. Give free rein to your creativity and experience to make this space vibrant and engaging!</p>
                 <h2 className="title title_mb-30">Create article</h2>
-                <div className="form editor__form" style={{flexDirection: 'column', width: '100%', height: '500px'}}>
+                <div className="form__editor">
                     <input type="text" name="title" placeholder="Title your article" className="editor__title"/>
-                    {/* <RichTextEditor/> */}
                     <JoditEditor />
+                    <div className="add-tag__wrapper">
+                        <div className="add-teg_input">
+                            <input type="text" name="tag" placeholder="Tag your article" className="editor__tag"/>
+                            <button className="button button__add-tag">+</button>
+                        </div>
+                        <p className="tags__info">You added the following tags:</p>
+                        <button className="button button__remove-tag">solid</button>
+                    </div>
+                    <button className="button button__publish">publish</button>
                 </div>
             </div>
         </section>
